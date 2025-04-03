fdcafb19ad4c68286f76acfc7abc97b5cb96be5f
Alec Volkert <alvo6266@colorado.edu>
2025-04-03 15:09:45 -0600

    update for meeting log 3

--- project_source_code/src/resources/js/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(
-  'https://your-project-id.supabase.co',
-  'your-anon-or-service-role-key' // service_role if uploading securely from backend
-);
-
-module.exports = supabase;
