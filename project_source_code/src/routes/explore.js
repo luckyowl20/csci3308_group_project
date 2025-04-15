96e9e0f984121abf62a3d4e5f7716c9395521086
Daniel <daniel.zhu@colorado.edu>
2025-04-15 14:58:20 -0600

    comments and removed useless code

--- project_source_code/src/routes/explore.js
+++ project_source_code/src/routes/explore.js
@@ -12,13 +12,16 @@ router.get('/', isAuthenticated, async (req, res) => {
   }
 });
 
-// GET /restaurants/loc - Show nearby restaurants based on user location
+// GET /restaurants/loc - Renders restaurants page to show nearby restaurants based on user location
 router.get('/restaurants/loc', isAuthenticated, async (req, res) => {
+  // Get lat long from request
   const { lat, lon } = req.query;
   if (!lat || !lon) return res.status(400).send("Missing coordinates.");
 
+  // Get resturant information
   const restaurants = await getNearbyRestaurants(lat, lon);
 
+  // Render page with lat, lon, 
   res.render('pages/restaurants', {
     lat, lon, restaurants
   });
@@ -28,10 +31,12 @@ router.get('/restaurants/loc', isAuthenticated, async (req, res) => {
 
 const axios = require('axios');
 
+// Query api for restaurant based on lat and lon
 async function getNearbyRestaurants(lat, lon) {
   const apiKey = process.env.MAP_API_KEY;
   const radius = 2000; // meters
 
+  // Api request structure
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=${radius}&type=restaurant&key=${apiKey}`;
 
   try {
@@ -42,7 +47,8 @@ async function getNearbyRestaurants(lat, lon) {
       rating: place.rating,
       photo: place.photos?.[0]?.photo_reference
       ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${apiKey}`
-      : null
+      : null,
+      place_id: place.place_id,
     }));
   } catch (error) {
     console.error('Google Places API error:', error.response?.data || error.message);
