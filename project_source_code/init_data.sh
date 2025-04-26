dda61decdeac2383654d58c0027e253aeb0e7b02
matt schneider <mdschneider88@gmail.com>
2025-04-25 19:05:26 -0600

    removed init_data sh file

--- project_source_code/init_data.sh
+++
@@ -1,6 +0,0 @@
-PG_URI="postgresql://lucky_moment_db_9qd6_user:maj5q8kNWO7zq34lzwSkkx7P9prsFk3c@dpg-d048opadbo4c73ec7dng-a.oregon-postgres.render.com/lucky_moment_db_9qd6"
-# Execute each .sql file in the directory
-for file in init_data/*.sql; do
-    echo "Executing $file..."
-    psql $PG_URI -f "$file"
-done
